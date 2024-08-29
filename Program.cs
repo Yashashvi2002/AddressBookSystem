cc79e614587025f547bd630379469b2c610ccedf
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-29 15:10:41 +0530

    Ability to create Contact in Address book

--- Program.cs
+++ Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
-    class AddressBookMain
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class AddressBook
+    {
+        public List<Contacts> contactList;
+        public AddressBook()
+        {
+            contactList = new List<Contacts>();
+        }
+
+        public void AddContact(Contacts contact)
+        {
+            contactList.Add(contact);
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in contactList)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Address:  {contact.Address}");
+                Console.WriteLine($"City: {contact.City} ");
+                Console.WriteLine($"State: {contact.State}");
+                Console.WriteLine($"Zip: {contact.Zip}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+            }
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            Contacts contact = new Contacts();
+            contact.FirstName = "Nik";
+            contact.LastName = "Dev";
+            contact.Address = "Chembur";
+            contact.City = "Mumbai";
+            contact.State = "Maharashtra";
+            contact.Zip = "400071";
+            contact.PhoneNumber = "1234567890";
+            contact.Email = "nikdev123.com";
+            addressBook.AddContact(contact);
+            addressBook.DisplayContacts();
+
+            
         }
     }
 }
