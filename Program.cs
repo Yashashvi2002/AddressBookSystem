627b5cc8b42691f2171d39f6f0a8f8189fb0db42
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-29 16:54:09 +0530

    Ability to add a new Contact to Address Book

--- Program.cs
+++ Program.cs
@@ -1,12 +1,145 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
-    class AddressBookMain
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class AddressBook
+    {
+        public List<Contacts> contactList;
+        public AddressBook()
+        {
+            contactList = new List<Contacts>();
+        }
+
+        public void AddContact(Contacts contact)
+        {
+            contactList.Add(contact);
+        }
+
+        public Contacts GetContactDetailsFromUser()
+        {
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            return new Contacts
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+                PhoneNumber = phoneNumber,
+                Email = email
+            };
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in contactList)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Address:  {contact.Address}");
+                Console.WriteLine($"City: {contact.City} ");
+                Console.WriteLine($"State: {contact.State}");
+                Console.WriteLine($"Zip: {contact.Zip}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            bool addMoreContacts = true;
+            while (addMoreContacts)
+            {
+                Console.Write("Do you want to add another contact? (yes/no): ");
+                string response = Console.ReadLine().ToLower();
+
+                if (response == "yes")
+                {
+                    Contacts newContact = addressBook.GetContactDetailsFromUser();
+                    addressBook.AddContact(newContact);
+                }
+                else if (response == "no")
+                {
+                    addMoreContacts = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+                }
+            }
+
+            //Contacts contact1 = new Contacts
+            //{
+            //    FirstName = "Nikhil",
+            //    LastName = "Dev",
+            //    Address = "Chembur",
+            //    City = "Mumbai",
+            //    State = "Maharashtra",
+            //    Zip = "400071",
+            //    PhoneNumber = "1234567890",
+            //    Email = "nikdev123.com",
+            //};
+
+            //Contacts contact2 = new Contacts
+            //{
+            //    FirstName = "Rahul",
+            //    LastName = "Sharma",
+            //    Address = "Kurla",
+            //    City = "Mumbai",
+            //    State = "Maharashtra",
+            //    Zip = "400072",
+            //    PhoneNumber = "2233445687",
+            //    Email = "rahulsharma123.com",
+            //};
+            //addressBook.AddContact(contact1);
+            //addressBook.AddContact(contact2);
+            addressBook.DisplayContacts();
+
+
         }
     }
 }
+
